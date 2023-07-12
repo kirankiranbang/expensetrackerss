354ef2d5e5badef397e08d350438291ad269e96b
kirankiranbang <105356674+kirankiranbang@users.noreply.github.com>
2023-07-12 18:35:27 +0530

    dfgdfg

--- util/database.js
+++ util/database.js
@@ -1,19 +1,3 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize('node-completes', 'root', 'Sept@45454545', {
-//   dialect: 'mysql',
-//   host: 'localhost'
-// });
-// sequelize.authenticate().then(() => {
-// // try{
-//   console.log('Connection has been established successfully (from util(folder) database.js).');
-// })
-
-// .catch((error) => {
-//   console.error('Unable to connect to the database: ', error);
-// });
-
-// module.exports = sequelize;
 
 
 const Sequelize = require('sequelize');
